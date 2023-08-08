4009edd262ed873ebd982b14afc226f04676ef7e
jonathan <jonathan_sm@mail.ru>
2023-08-08 14:15:51 +0300

    Opp полиморфизм инкапсуляция наследование

---
+++ src/Truck.java
@@ -0,0 +1,14 @@
+public class Truck extends Car {
+
+
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void doService() {
+        super.doService();
+        checkTrailer();
+    }
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+}
